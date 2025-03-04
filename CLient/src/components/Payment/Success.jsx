a855687c35fee26c2f555cafa05fa05d2d4f4868
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-03-04 23:12:39 +0530

    Increase loading time on successful payment

--- CLient/src/components/Payment/Success.jsx
+++ CLient/src/components/Payment/Success.jsx
@@ -7,28 +7,35 @@ import Footer from "../Footer/Footer";
 import { useEffect, useState } from "react";
 import toastMessage from "../ToastMessage";
 const Success = () => {
-
   const { cartItems, clearCartItems, user, deliveryCharge } = useUserContext();
   const [loader, setLoader] = useState(true);
-  const [discount, setDiscount] = useState(JSON.parse(localStorage.getItem("discount")));
-  const [totalCartItemPrice, setTotalCartItemPrice] = useState(JSON.parse(localStorage.getItem("totalCartItemPrice")));
+  const [discount, setDiscount] = useState(
+    JSON.parse(localStorage.getItem("discount"))
+  );
+  const [totalCartItemPrice, setTotalCartItemPrice] = useState(
+    JSON.parse(localStorage.getItem("totalCartItemPrice"))
+  );
   useEffect(() => {
-
     const placeOrder = async () => {
       try {
-        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/order/placeOrder`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            user,
-            restaurant: cartItems[0].restaurant,
-            address: localStorage.getItem("address"),
-            cartItems,
-            totalAmount: discount ? totalCartItemPrice + deliveryCharge - discount : totalCartItemPrice + deliveryCharge,
-          }),
-        });
+        const response = await fetch(
+          `${process.env.REACT_APP_BACKEND_URL}/api/order/placeOrder`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              user,
+              restaurant: cartItems[0].restaurant,
+              address: localStorage.getItem("address"),
+              cartItems,
+              totalAmount: discount
+                ? totalCartItemPrice + deliveryCharge - discount
+                : totalCartItemPrice + deliveryCharge,
+            }),
+          }
+        );
 
         const data = await response.json();
         if (!data.success) {
@@ -42,37 +49,47 @@ const Success = () => {
           localStorage.removeItem("totalCartItemPrice");
           localStorage.removeItem("address");
           clearCartItems();
-        }, 2000);
+        }, 5000);
       } catch (error) {
         console.log(error);
         toastMessage({ msg: error.message, type: "error" });
       }
-    }
+    };
 
     placeOrder();
   }, []);
 
-  return (
-    loader ? <div className="payment-success-loader">
-
-    </div> :
-      <>
-        <Navbar />
-        <div className="container" style={{ marginTop: "100px" }}>
-          <div className="success-page">
-            <img src="https://www.kablooe.com/wp-content/uploads/2019/08/check_mark.png" alt="success" />
-            <h2>Payment Successful!</h2>
-            <p>Thank you! Your payment of <FormatPrice price={discount ? totalCartItemPrice + deliveryCharge - discount : totalCartItemPrice + deliveryCharge} /> has been received.</p>
-            <NavLink to="/">
-              <button className="btn">
-                Back to Home
-              </button>
-            </NavLink>
-          </div>
+  return loader ? (
+    <div className="payment-success-loader"></div>
+  ) : (
+    <>
+      <Navbar />
+      <div className="container" style={{ marginTop: "100px" }}>
+        <div className="success-page">
+          <img
+            src="https://www.kablooe.com/wp-content/uploads/2019/08/check_mark.png"
+            alt="success"
+          />
+          <h2>Payment Successful!</h2>
+          <p>
+            Thank you! Your payment of{" "}
+            <FormatPrice
+              price={
+                discount
+                  ? totalCartItemPrice + deliveryCharge - discount
+                  : totalCartItemPrice + deliveryCharge
+              }
+            />{" "}
+            has been received.
+          </p>
+          <NavLink to="/">
+            <button className="btn">Back to Home</button>
+          </NavLink>
         </div>
-        <Footer />
-      </>
-  )
-}
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Success
+export default Success;
